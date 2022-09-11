4326649c8318e8b028fcd5944e0c5da3a31ede6f
Volodymyr <Volodymyr.Kryvtsov-external@sema4genomics.com>
2022-09-11 14:12:13 -0400

    Create TodoList using Prisma: add rest apis

---
+++ api/todos/index.ts
@@ -0,0 +1,63 @@
+import {Router} from "express";
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+const router = Router();
+
+router.get('/', async (req, res) => {
+    const todos = await prisma.todo.findMany();
+    res.json(todos)
+})
+
+router.get('/:id', async (req, res) => {
+    const todo = await prisma.todo.findFirst({
+        where: {id: parseInt(req.params.id)}
+    });
+    res.json(todo)
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const {body, dueDate = new Date(), title} = req.body;
+        await prisma.todo.create({
+            data: {
+                body, dueDate, title
+            }
+        });
+        res.json({success: true})
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const {body, dueDate = new Date(), title, isComplete} = req.body;
+        await prisma.todo.update({
+            where: {
+                id: +req.params.id
+            },
+            data: {
+                body, dueDate, title, isComplete
+            }
+        });
+        res.json({success: true})
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        await prisma.todo.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        res.json({success: true})
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+export default router;
---
+++ server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import api from "./api";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use('/api', api);
+app.listen(1337, () => console.log('server is running'))
